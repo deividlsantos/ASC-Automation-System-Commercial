1b3e9d0104dfa2130052c739ee9b8ae25dce221d
deividlsantos <deividluiz.santos@hotmail.com>
2018-09-10 00:59:31 -0300

    create table clients

--- AutomationCommercial.sql
+++ AutomationCommercial.sql
@@ -16,3 +16,15 @@ id bigint primary key auto_increment,
 descricao varchar(50) not null,
 teto_mini decimal not null,
 teto_max decimal not null);
+
+create table clientes(
+id bigint primary key auto_increment,
+id_endereco bigint,
+nome varchar(45) not null,
+rg varchar(10) not null,
+cpf varchar(11) not null,
+email varchar(50) not null,
+telefone varchar(10),
+celular varchar(11) not null,
+foreign key (id_endereco) references endereco(id));
+
