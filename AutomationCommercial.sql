a34198840004ea89446c9df48fc538dbf16fbf9f
julianogoncalves23 <juliano.a.goncalves@gmail.com>
2018-09-14 20:35:24 -0300

    tabela fornecedor

--- AutomationCommercial.sql
+++ AutomationCommercial.sql
@@ -1,30 +1,150 @@
-create database AutomationCommercial;
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: automationcommercial
+-- ------------------------------------------------------
+-- Server version	8.0.12
 
-use AutomationCommercial;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table endereco(
-id bigint primary key auto_increment,
-rua varchar(50) not null,
-numero int not null,
-bairro varchar(50) not null,
-cidade varchar(50) not null,
-estado varchar(45) not null,
-pais varchar(45) not null);
+--
+-- Table structure for table `cargo`
+--
 
-create table cargo(
-id bigint primary key auto_increment,
-descricao varchar(50) not null,
-teto_mini decimal not null,
-teto_max decimal not null);
+DROP TABLE IF EXISTS `cargo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cargo` (
+  `id_cargo` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) NOT NULL,
+  `teto_mini` decimal(10,0) NOT NULL,
+  `teto_max` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id_cargo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table clientes(
-id bigint primary key auto_increment,
-id_endereco bigint,
-nome varchar(45) not null,
-rg varchar(10) not null,
-cpf varchar(11) not null,
-email varchar(50) not null,
-telefone varchar(10),
-celular varchar(11) not null,
-foreign key (id_endereco) references endereco(id));
+--
+-- Dumping data for table `cargo`
+--
 
+LOCK TABLES `cargo` WRITE;
+/*!40000 ALTER TABLE `cargo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cargo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cliente` (
+  `id_cliente` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `rg` int(9) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefone` int(10) DEFAULT NULL,
+  `celular` int(11) NOT NULL,
+  `id_endereco_fk` bigint(20) NOT NULL,
+  PRIMARY KEY (`id_cliente`),
+  KEY `id_endereco_fk` (`id_endereco_fk`),
+  CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_endereco_fk`) REFERENCES `endereco` (`id_endereco`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `endereco` (
+  `id_endereco` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cep` int(8) NOT NULL,
+  `logradouro` varchar(50) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `pais` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_endereco`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `endereco`
+--
+
+LOCK TABLES `endereco` WRITE;
+/*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fornecedor`
+--
+
+DROP TABLE IF EXISTS `fornecedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `fornecedor` (
+  `id_fornecedor` bigint(20) NOT NULL AUTO_INCREMENT,
+  `razao_social` varchar(45) NOT NULL,
+  `nome_fantasia` varchar(45) NOT NULL,
+  `cnpj` int(14) NOT NULL,
+  `telefone` int(10) NOT NULL,
+  `celular` int(11) NOT NULL,
+  `id_endereco_fk` bigint(20) NOT NULL,
+  PRIMARY KEY (`id_fornecedor`),
+  KEY `id_endereco_fk` (`id_endereco_fk`),
+  CONSTRAINT `fornecedor_ibfk_1` FOREIGN KEY (`id_endereco_fk`) REFERENCES `endereco` (`id_endereco`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fornecedor`
+--
+
+LOCK TABLES `fornecedor` WRITE;
+/*!40000 ALTER TABLE `fornecedor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fornecedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'automationcommercial'
+--
+
+--
+-- Dumping routines for database 'automationcommercial'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-14 20:18:46
